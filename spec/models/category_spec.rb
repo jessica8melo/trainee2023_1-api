68690d497e76ecd5afee7602faea985b6baf7523
jessica8melo <jessica8melo@gmail.com>
2023-05-29 20:58:07 -0300

    ✅ testing category factory

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "Testing factory" do
+    it "should be valid" do
+      expect(build(:category)).to be_valid
+    end
+  end
 end

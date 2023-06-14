fe0b655a48c2c1b118065544c9226d7485f06663
pafev <paulofvilarimda@gmail.com>
2023-06-14 02:07:03 -0300

    login and token_authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
+    def login
+        user = User.find_by(email: login_params[:email])
+        if user.valid_password?(login_params[:password])
+            render json: user, status: :ok
+        else
+            head :unauthorized
+        end
+    rescue StandardError => e
+        head :unauthorized
+    end
 
+    private
+    def login_params
+        params.require(:user).permit(:email, :password)
+    end
 end

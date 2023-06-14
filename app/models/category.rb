5c11c4835990babe6e20de9d068c6bf33bef1542
pafev <paulofvilarimda@gmail.com>
2023-06-14 02:23:20 -0300

    default value is_admin, remove validates category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ApplicationRecord
     validates :name, presence: :true, uniqueness: :true
-    validates :description, uniqueness: :true
 
     has_many :post_categories
     has_many :posts, through: :post_category
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  after_initialize :set_defaults
   validates :name, presence: :true
   
   has_many :posts
   has_many :commentaries
   has_many :feedbacks
+  
+  private
+  def set_defaults
+    is_admin = false
+  end
 end

a3784f1eccaf07bca714369f19c76195d68e238d
pafev <paulofvilarimda@gmail.com>
2023-06-14 02:32:28 -0300

    populando seed e ajustando nxn postcategory

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
     validates :name, presence: :true, uniqueness: :true
 
     has_many :post_categories
-    has_many :posts, through: :post_category
+    has_many :posts, through: :post_categories
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ApplicationRecord
 
     belongs_to :user, -> { where is_admin: true }
     has_many :post_categories
-    has_many :categories, through: :post_category
+    has_many :categories, through: :post_categories
     has_many :commentaries
 end
